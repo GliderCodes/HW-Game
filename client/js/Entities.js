bdf8717d4978f110572ecf7c67e82000b602a660
GliderCodes <thegoglider@gmail.com>
2019-11-25 12:26:35 +0400

    Entities fix

--- client/js/Entities.js
+++ client/js/Entities.js
@@ -1,183 +1,302 @@
-// Loading necessary modules with Node.js
-const express = require('express')
-const app = express();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const path = require('path');
-const morgan = require('morgan');
-const cons = require('consolidate');
-const pageRouter = require('./router/pages')
-const RedisStore = require("connect-redis")(session);
+
+var initPack = {player:[],bullet:[]};
+var removePack = {player:[],bullet:[]};
 
 
 
-// Constant variables important for assignments
-const config = require('./config.json');
-const client = path.resolve("../client")
-const port = config.port;
-const debug = config.debug;
+Entity = function(param){
+	var self = {
+		x:250,
+		y:250,
+		spdX:0,
+		spdY:0,
+		id:"",
+		map:'forest',
+	}
+	if(param){
+		if(param.x)
+			self.x = param.x;
+		if(param.y)
+			self.y = param.y;
+		if(param.map)
+			self.map = param.map;
+		if(param.id)
+			self.id = param.id;
+	}
 
-// Mongoose database configuration
-const mongoose = require("mongoose");
-const Player = require('./core/playerSchema')
-
-// Mongoose database connection
-mongoose.connect('mongodb://localhost/my_database', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-
-// Setting morgan to load information about the requests for development/configuration uses.
-app.use(morgan('dev'));
-
-// Initialize bodyParser to parse the incoming parameter requests to req.body
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-// Initialize cookieParser to allow us to access the cookies stored in the browser.
-app.use(cookieParser());
-app.use(express.static(path.resolve(client)));
-
-var sessionMiddleware =
-    session({
-        secret: 'user_sid',
-        resave: true,
-        saveUninitialized: false,
-        cookie: {
-            httpOnly: true,
-            secure: false,
-            maxAge: 10 * 60 * 1000
-        }
-    })
-io.use(function (socket, next) {
-    sessionMiddleware(socket.request, socket.request.res, next);
-});
-
-// Using session
-app.use(sessionMiddleware)
-
-// Setting the express static and files for client
-app.engine('html', cons.swig)
-app.set('views', path.resolve("../client/views"))
-app.set('view engine', 'html');
-
-// Using routers 
-app.use('/', pageRouter)
-
-// Errors => page not found 404
-app.use((req, res, next) => {
-    var err = new Error('Page not found');
-    err.status = 404;
-    next(err);
-})
-
-// Handling errors (send them to the client)
-app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send(err.message);
-});
-
-
-// server events and listening
-server.on("error", (err) => {
-    console.log('Server Error: ' + err);
-});
-
-// listening to the port
-server.listen(port, () => {
-    console.log(`Listening at port ${port}...`);
-});
-
-function valueExists(jsObj, value, cb){
-    for (var key in jsObj){
-        if (jsObj[key].username == value) 
-        return cb(key, true);
-    }
-    return cb(key, false);
+	self.update = function(){
+		self.updatePosition();
+	}
+	self.updatePosition = function(){
+		self.x += self.spdX;
+		self.y += self.spdY;
+	}
+	self.getDistance = function(pt){
+		return Math.sqrt(Math.pow(self.x-pt.x,2) + Math.pow(self.y-pt.y,2));
+	}
+	return self;
 }
-// socket connection for players/clients
-var players = {};
-io.on('connection', function (socket) {
-    
-    var playerdb = socket.request.session.user
-    // console.log(players)
-    if (playerdb)
-    valueExists(players, playerdb.username, function(key, exists) {
-        if (exists) {
-            // console.log(players[key])
-            delete players[key] 
-            players[socket.id] = {
-                username: playerdb.username,
-                x: playerdb.x,
-                y: playerdb.y,
-                score: 0
-            };
+Entity.getFrameUpdateData = function(){
+	var pack = {
+		initPack:{
+			player:initPack.player,
+			bullet:initPack.bullet,
+		},
+		removePack:{
+			player:removePack.player,
+			bullet:removePack.bullet,
+		},
+		updatePack:{
+			player:Player.update(),
+			bullet:Bullet.update(),
+		}
+	};
+	initPack.player = [];
+	initPack.bullet = [];
+	removePack.player = [];
+	removePack.bullet = [];
+	return pack;
+}
 
-        } else {
-            players[socket.id] = {
-                username: playerdb.username,
-                x: playerdb.x,
-                y: playerdb.y,
-                score:0
-            };
-        }
-    })
-    console.log(players)
 
-    // Socket listener for when players disconnect
-    socket.on('disconnect', function () {
-        console.log("Disconnected")
-        if (players[socket.id] && players[socket.id].x != undefined && players[socket.id].y != undefined) {
-            Player.findOneAndUpdate({_id: playerdb._id}, {x:players[socket.id].x, y:players[socket.id].y})
-        }
-        delete players[socket.id];
-    });
+Player = function(param){
+	var self = Entity(param);
+	self.number = "" + Math.floor(10 * Math.random());
+	self.username = param.username;
+	self.pressingRight = false;
+	self.pressingLeft = false;
+	self.pressingUp = false;
+	self.pressingDown = false;
+	self.pressingAttack = false;
+	self.mouseAngle = 0;
+	self.maxSpd = 10;
+	self.hp = 10;
+	self.hpMax = 10;
+	self.score = 0;
 
-    // Socket listener for when players move
-    socket.on('movement', function (data) {
-        var player = players[socket.id];
-        if (data.left) {
-            player.x -= 5;
-        }
-        if (data.up) {
-            player.y -= 5;
-        }
-        if (data.right) {
-            player.x += 5;
-        }
-        if (data.down) {
-            player.y += 5;
-        }
-    });
-    socket.on('storeOldScore', function(oldScore) {
-        console.log("Storing Score: "+oldScore)
-        if (players[socket])
-            players[socket.id].score = oldScore
-    })
-    socket.on('onDeath', function(param) {
-        // console.log(socket)
-        // console.log(players[socket.id])
-        if (players[socket.id] != undefined)
-        var playerUsername = players[socket.id].username
-        
-        Player.findOne({username:playerUsername}, function(err, player) {
-            if (player.score < param) {
-                socket.send(param)
-                Player.findOneAndUpdate({username: playerUsername}, {score: param}, (err) => {
-                    console.log(err)
-                })
-        }else {
-            socket.send(player.score)
-        }})
-        
-    })
-});
-setInterval(function () {
-    io.sockets.emit('state', players);
-    
-}, 1000 / 60);
+	var super_update = self.update;
+	self.update = function(){
+		self.updateSpd();
+
+		super_update();
+
+		if(self.pressingAttack){
+			self.shootBullet(self.mouseAngle);
+		}
+	}
+	self.shootBullet = function(angle){
+		Bullet({
+			parent:self.id,
+			angle:angle,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		});
+	}
+
+	self.updateSpd = function(){
+		if(self.pressingRight)
+			self.spdX = self.maxSpd;
+		else if(self.pressingLeft)
+			self.spdX = -self.maxSpd;
+		else
+			self.spdX = 0;
+
+		if(self.pressingUp)
+			self.spdY = -self.maxSpd;
+		else if(self.pressingDown)
+			self.spdY = self.maxSpd;
+		else
+			self.spdY = 0;
+	}
+
+	self.getInitPack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			number:self.number,
+			hp:self.hp,
+			hpMax:self.hpMax,
+			score:self.score,
+			map:self.map,
+		};
+	}
+	self.getUpdatePack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			hp:self.hp,
+			score:self.score,
+			map:self.map,
+		}
+	}
+
+	Player.list[self.id] = self;
+
+	initPack.player.push(self.getInitPack());
+	return self;
+}
+Player.list = {};
+Player.onConnect = function(socket,username,progress){
+	var map = 'forest';
+	if(Math.random() < 0.5)
+		map = 'field';
+	var player = Player({
+		username:username,
+		id:socket.id,
+		map:map,
+		socket:socket,
+		progress:progress,
+	});
+
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		else if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
+		else if(data.inputId === 'attack')
+			player.pressingAttack = data.state;
+		else if(data.inputId === 'mouseAngle')
+			player.mouseAngle = data.state;
+	});
+
+	socket.on('changeMap',function(data){
+		if(player.map === 'field')
+			player.map = 'forest';
+		else
+			player.map = 'field';
+	});
+
+	socket.on('sendMsgToServer',function(data){
+		for(var i in SOCKET_LIST){
+			SOCKET_LIST[i].emit('addToChat',player.username + ': ' + data);
+		}
+	});
+	socket.on('sendPmToServer',function(data){ //data:{username,message}
+		var recipientSocket = null;
+		for(var i in Player.list)
+			if(Player.list[i].username === data.username)
+				recipientSocket = SOCKET_LIST[i];
+		if(recipientSocket === null){
+			socket.emit('addToChat','The player ' + data.username + ' is not online.');
+		} else {
+			recipientSocket.emit('addToChat','From ' + player.username + ':' + data.message);
+			socket.emit('addToChat','To ' + data.username + ':' + data.message);
+		}
+	});
+
+	socket.emit('init',{
+		selfId:socket.id,
+		player:Player.getAllInitPack(),
+		bullet:Bullet.getAllInitPack(),
+	})
+}
+Player.getAllInitPack = function(){
+	var players = [];
+	for(var i in Player.list)
+		players.push(Player.list[i].getInitPack());
+	return players;
+}
+
+Player.onDisconnect = function(socket){
+	let player = Player.list[socket.id];
+	if(!player)
+		return;
+	Database.savePlayerProgress({
+		username:player.username,
+	});
+	delete Player.list[socket.id];
+	removePack.player.push(socket.id);
+}
+Player.update = function(){
+	var pack = [];
+	for(var i in Player.list){
+		var player = Player.list[i];
+		player.update();
+		pack.push(player.getUpdatePack());
+	}
+	return pack;
+}
+
+
+Bullet = function(param){
+	var self = Entity(param);
+	self.id = Math.random();
+	self.angle = param.angle;
+	self.spdX = Math.cos(param.angle/180*Math.PI) * 10;
+	self.spdY = Math.sin(param.angle/180*Math.PI) * 10;
+	self.parent = param.parent;
+
+	self.timer = 0;
+	self.toRemove = false;
+	var super_update = self.update;
+	self.update = function(){
+		if(self.timer++ > 100)
+			self.toRemove = true;
+		super_update();
+
+		for(var i in Player.list){
+			var p = Player.list[i];
+			if(self.map === p.map && self.getDistance(p) < 32 && self.parent !== p.id){
+				p.hp -= 1;
+
+				if(p.hp <= 0){
+					var shooter = Player.list[self.parent];
+					if(shooter)
+						shooter.score += 1;
+					p.hp = p.hpMax;
+					p.x = Math.random() * 500;
+					p.y = Math.random() * 500;
+				}
+				self.toRemove = true;
+			}
+		}
+	}
+	self.getInitPack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		};
+	}
+	self.getUpdatePack = function(){
+		return {
+			id:self.id,
+			x:self.x,
+			y:self.y,
+		};
+	}
+
+	Bullet.list[self.id] = self;
+	initPack.bullet.push(self.getInitPack());
+	return self;
+}
+Bullet.list = {};
+
+Bullet.update = function(){
+	var pack = [];
+	for(var i in Bullet.list){
+		var bullet = Bullet.list[i];
+		bullet.update();
+		if(bullet.toRemove){
+			delete Bullet.list[i];
+			removePack.bullet.push(bullet.id);
+		} else
+			pack.push(bullet.getUpdatePack());
+	}
+	return pack;
+}
+
+Bullet.getAllInitPack = function(){
+	var bullets = [];
+	for(var i in Bullet.list)
+		bullets.push(Bullet.list[i].getInitPack());
+	return bullets;
+}
